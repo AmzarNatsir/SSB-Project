d52bbfe6497a0cef3c39b25fc26efd5e56a9ed04
Amzar Natsir <53068228+AmzarNatsir@users.noreply.github.com>
2026-05-29 08:58:23 +0800

    update home page - current date

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,12 +14,12 @@
             <!-- Page Header -->
             <div class="d-flex align-items-center justify-content-between gap-2 mb-4 flex-wrap">
                 <div>
-                    <h4 class="mb-0">Project - Dashboard</h4>
+                    <h4 class="mb-0">Project - Home</h4>
                 </div>
                 <div class="gap-2 d-flex align-items-center flex-wrap">
-                    <div class="daterangepick form-control w-auto d-flex align-items-center">
+                    <div class="form-control w-auto d-flex align-items-center">
                         <i class="ti ti-calendar text-dark me-2"></i>
-                        <span class="reportrange-picker-field text-dark">23 May 2025 - 30 May 2025</span>
+                        <span class="text-dark">{{ \Carbon\Carbon::now()->locale('id')->translatedFormat('l, d F Y') }}</span>
                     </div>
                     <a href="javascript:void(0);" class="btn btn-icon btn-outline-light shadow" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Refresh" data-bs-original-title="Refresh"><i class="ti ti-refresh"></i></a>
                     <a href="javascript:void(0);" class="btn btn-icon btn-outline-light shadow" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Collapse" data-bs-original-title="Collapse" id="collapse-header"><i class="ti ti-transition-top"></i></a>
